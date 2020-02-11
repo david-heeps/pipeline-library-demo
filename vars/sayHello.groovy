f2328b0ef4a77ba3c1143b969928a0c945c737c4
david-heeps <47637139+david-heeps@users.noreply.github.com>
2020-02-11 04:57:37 -0500

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
-  echo "Hello, ${name}."
+def call(Map config) {
+  git url: "https://github.com/david-heeps/${config.name}-worldpub"
 }
 
