f0d2e77f04f74a28dab63d46c7fc47199730dbaf
david-heeps <47637139+david-heeps@users.noreply.github.com>
2020-02-12 00:10:49 -0500

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -4,4 +4,7 @@ def call(Map config) {
   git url: "https://github.com/david-heeps/${config.name}-worldpub"
   sh './jtest.sh'
 }
-
+def call(Map config2) {
+  git url: "https://github.com/david-heeps/${config2.name}-test2"
+  sh './bashtest2.sh'
+}
