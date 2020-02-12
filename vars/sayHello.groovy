e9f2c5ea6da55207bddc5d6859ccc3c588403b56
david-heeps <47637139+david-heeps@users.noreply.github.com>
2020-02-12 00:54:57 -0500

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -4,7 +4,7 @@ def call(def config, def config2) {
   git url: "https://github.com/david-heeps/${config.name}-worldpub"
   sh './jtest.sh'
   
-  git url: "https://github.com/david-heeps/${config.name2}-test2"
+  git url: "https://github.com/david-heeps/${config2.name2}-test2"
   sh './bashtest2.sh'
 }
 
